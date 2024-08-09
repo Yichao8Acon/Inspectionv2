c1d3fbca0fac5abfb2dcb806789fc2228095d8c9
Yichao8Acon <yichao.qin@aconlabs.com>
2024-08-09 11:25:48 -0700

    Initial

--- src/ObjectLocator.hpp
+++ src/ObjectLocator.hpp
@@ -6,7 +6,13 @@
 class ObjectLocator {
 
 public:
-
+    /**
+    * Blur and filter out selected HSV color
+    *
+    * @param input source image loaded in cv::Mat
+    * @return processed image
+    *
+    */
     cv::Mat computeMask(cv::Mat &srcImg);
 
     std::vector<cv::RotatedRect> findRectangleVects(cv::Mat &srcImg);
